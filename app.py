2070ea0a1aa5ce747d74f924c0dee09fea458090
dana367 <dana.larina1@gmail.com>
2024-12-26 11:09:56 +0000

    setting up database file

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from quart import Quart
+from quart_db import QuartDB
 
 app = Quart(__name__)
+QuartDB(app, url="sqlite:///database.db")
 
 
 @app.get("/")
 async def hello():
-    return "Hello, World!"
+    return {"Hello": "World!"}
